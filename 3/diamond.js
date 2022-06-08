19bef0e65e7f617aa473b6cd3ff5defdf9aeea3a
simopunkc <pembela.allah@gmail.com>
2022-06-08 08:45:11 +0700

    improve algorithm chall 3

--- 3/diamond.js
+++ 3/diamond.js
@@ -1,19 +1,19 @@
 const getDiamond = (input) => {
-  let col = 0
-  for(let i=1;i<=input;i++){
-    col += (i==1) ? 1 : 2
-  }
-  const center = Math.ceil(col/2)
-  let start, finish, temp="", curr=0
-  for(let i=1;i<=col;i++){
-    start = center-curr
-    finish = center+curr
-    curr = (i<center) ? curr+1 : curr-1
-    for(let j=1;j<=col;j++){
-      temp += (j>=start && j<=finish) ? "-" : "+"
+  let col = (input * 2) - 1
+  let start, finish, temp = ""
+  for (let i = 0; i < col; i++) {
+    if (i <= input - 1) {
+      start = (input - 1) - i
+      finish = (input - 1) + i
+    } else {
+      start = (i - input) + 1
+      finish = (col - start) - 1
+    }
+    for (let j = 0; j < col; j++) {
+      temp += (j >= start && j <= finish) ? "-" : "+"
     }
     console.log(temp)
-    temp=""
+    temp = ""
   }
 }
 
